bbc97c6b194c26b3d4ec1fe9e18f3d6c109548de
Simic46 <aleksandar.simic.vr@gmail.com>
2021-07-28 12:36:31 +0200

    Implement Routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "../assets/styles/components/Header.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <h1>
-        <span>AIR</span>vironment
-      </h1>
+      <div className="header-nav">
+        <Link to="/">
+          <h1>
+            <span>AIR</span>vironment
+          </h1>
+        </Link>
+      </div>
+
       <div className="nav">
         <span>ALL DATA</span>
         <span>AVERAGES</span>
